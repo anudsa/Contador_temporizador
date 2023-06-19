449f0b100e818894b9be5fd19b6b4edf83113f72
anudsa <anwardelgadosilva@gmail.com>
2023-06-18 23:08:51 -0600

    Se termina la parte 1, contador

--- sources/main.js
+++ sources/main.js
@@ -1,1 +1,42 @@
+// se crean constantes de los botones y el contador
+const decreaseButton = document.getElementById("decrease");
+const stopButton = document.getElementById("stop");
+const increaseButton = document.getElementById("increase");
+const paragraph = document.getElementById("counterParagraph")
+
+let counter = 0;
+paragraph.textContent =  counter;
+let reset=0;
+let increase=0;
+let decrease=0;
+
+
+decreaseButton.addEventListener('click', async () => {
+    reset=0;
+    decrease=1;
+    increase=0;
+    while (reset == 0 && increase==0) {
+        counter--;
+        paragraph.textContent = counter;
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+});
+
+
+stopButton.addEventListener('click',async()=>{
+    counter=0;
+    paragraph.textContent = counter;
+    reset=1;
+})
+
+increaseButton.addEventListener('click', async () => {
+    reset=0;
+    increase=1;
+    decrease=0;
+    while (reset == 0 && decrease==0) {
+        counter++;
+        paragraph.textContent = counter;
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+});
 
